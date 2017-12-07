00369b89e2d77c324019ad8185b0e06ea1eb7aaf
Colin Cowles <colin@intrepid.io>
2017-12-08 00:38:36 +0900

    fixed logic issue

--- gomoku-swift/Controllers/GameBoardViewController.swift
+++ gomoku-swift/Controllers/GameBoardViewController.swift
@@ -10,10 +10,31 @@ import UIKit
 
 class GameBoardViewController: UIViewController {
     
+    var sizeOfGrid: Int
+    var target: Int
+    
+    init(gridSize: Int, target: Int) {
+        self.sizeOfGrid = gridSize
+        self.target = target
+        super.init(nibName: String(describing: GameBoardViewController.self), bundle: nil)
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        self.sizeOfGrid = 0
+        self.target = 0
+        assertionFailure("should only create GameBoardViewController with model info")
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.sizeOfGrid = 0
+        self.target = 0
+        assertionFailure("should only create GameBoardViewController with model info")
+        super.init(coder: aDecoder)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(navigationController)
         // Do any additional setup after loading the view.
     }
 
