ed7a6126e1975047ea9652731275f3f0517e83af
Colin Cowles <colin@intrepid.io>
2017-12-07 18:08:25 +0900

    first commit

---
+++ gomoku-swift/Models/BoardSquare.swift
@@ -0,0 +1,13 @@
+//
+//  BoardSquare.swift
+//  gomoku-swift
+//
+//  Created by Colin Cowles on 12/7/17.
+//  Copyright © 2017 Colin Cowles. All rights reserved.
+//
+
+import UIKit
+
+class BoardSquare: NSObject {
+
+}
---
+++ gomoku-swift/Views/BoardSquareView.swift
@@ -0,0 +1,22 @@
+//
+//  BoardSquareView.swift
+//  gomoku-swift
+//
+//  Created by Colin Cowles on 12/7/17.
+//  Copyright © 2017 Colin Cowles. All rights reserved.
+//
+
+import UIKit
+
+class BoardSquareView: UIView {
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var button: UIButton!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
